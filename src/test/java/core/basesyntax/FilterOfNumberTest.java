bf0234ef3ee67400d926a93e780b96febf293c64
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-13 15:24:30 +0300

    improved test

--- src/test/java/core/basesyntax/FilterOfNumberTest.java
+++ src/test/java/core/basesyntax/FilterOfNumberTest.java
@@ -1,37 +1,37 @@
 package core.basesyntax;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class FilterOfNumberTest {
+    private int[] array;
+
+    @Before
+    public void arrayInitializing() {
+        array = new int[40];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i - array.length / 2;
+        }
+    }
+
     @Test(expected = ArithmeticException.class)
     public void checkDivisionRemainder() {
-        Random random = new Random();
+
         FilterOfNumber checkRemainder = new FilterOfNumber();
         for (int value = -5; value < 10; value++) {
-            int size = random.nextInt(10 + 1) + 5;
-            int[] array = new int[size];
-            for (int i = 0; i < size; i++) {
-                array[i] = random.nextInt(40 + 1) - 20;
-            }
-
             int[] actualResult = checkRemainder.filterMultiplesOfNumber(array, value);
             int[] expectedResult = correctFilterMultiplesOfNumber(array, value);
             Assert.assertArrayEquals("Test failed with array: " + printArray(actualResult)
-                    + " exp: " + printArray(expectedResult)
-                    + " and value: " + value, actualResult, expectedResult);
+                    + "\nexpected array: " + printArray(expectedResult)
+                    + "\nand value: " + value, actualResult, expectedResult);
         }
     }
 
     private String printArray(int[] array) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            builder.append(array[i]).append(" ");
-        }
-        return builder.toString();
+        return array.toString();
     }
 
     private int[] correctFilterMultiplesOfNumber(int[] array, int value) {
