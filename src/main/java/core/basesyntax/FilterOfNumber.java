2bad2726b88399dbb89b3f113f72c0f260597403
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-09-23 17:59:27 +0300

    added tags

--- src/main/java/core/basesyntax/FilterOfNumber.java
+++ src/main/java/core/basesyntax/FilterOfNumber.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class FilterOfNumber {
 
     /**
-     * Реализуйте метод filterMultiplesOfNumber(int[] array, int multiplier), принимающий
+     * <p>Реализуйте метод filterMultiplesOfNumber(int[] array, int multiplier), принимающий
      * в качестве аргумента массив целых чисел `array` и число `multiplier`
-     * остаток от деления на которое необходимо проверить.
+     * остаток от деления на которое необходимо проверить.</p>
      *
-     * Ваш метод, должен вернуть массив, состоящий из чисел, которые делятся
-     * без остатка на `multiplier`
+     * <p>Ваш метод, должен вернуть массив, состоящий из чисел, которые делятся
+     * без остатка на `multiplier`</p>
      */
     public int[] filterMultiplesOfNumber(int[] array, int multiplier) {
         return null;
