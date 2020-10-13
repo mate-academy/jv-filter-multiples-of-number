64c87cec80767c5e9086bcf1fbd43eb01537bf8e
VovkAA <vovkandrew@gmail.com>
2020-10-13 12:24:26 +0300

    fixed

--- src/main/java/core/basesyntax/FilterOfNumber.java
+++ src/main/java/core/basesyntax/FilterOfNumber.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FilterOfNumber {
@@ -15,21 +13,6 @@ public class FilterOfNumber {
      * без остатка на `multiplier`</p>
      */
     public int[] filterMultiplesOfNumber(int[] array, int multiplier) {
-        /*int number = 0;
-        for (int i: array) {
-            if (i % multiplier == 0) {
-                number++;
-            }
-        }
-        int[] result = new int[number];
-        int index = 0;
-        for (int i: array) {
-            if (i % multiplier == 0) {
-                result[index] = i;
-                index++;
-            }
-        }
-        return result*/
         return IntStream.of(array).filter(i -> i % multiplier == 0).toArray();
     }
 }
